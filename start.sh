77ca53544a56213244707ec1fc3c91689c0874b5
Dwyor-tmx <86127970+Dwyor-tmx@users.noreply.github.com>
2024-06-12 14:48:35 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,6 @@ set -e
 #Credit to Meghthedev for the initial script 
 
 # init
-repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs
 
 # Run inside foss.crave.io devspace
 # Remove existing local_manifests
@@ -14,19 +13,18 @@ crave run --clean --no-patch -- "rm -rf .repo/local_manifests && \
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
 
 # sync tree
-git clone -b wip/round-corner-experimentations https://github.com/alternoegraha/device_xiaomi_fog device/xiaomi/fog && \
+git clone -b ArrowOS https://github.com/ardia-kun/device_xiaomi_fog device/xiaomi/fog && \
 
-# clone hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi && \
+git clone -b thirteen https://github.com/PixelExperience/hardware_xiaomi hardware/xiaomi && \
 
 # Set up build environment
-source build/envsetup.sh && \
+. build/envsetup.sh && \
 
 # Lunch configuration
-lunch lineage_fog-userdebug && \
+lunch arrow_fog-user && \
 
 # Build the ROM
-mka bacon"
+m bacon"
 
 # Pull generated zip files
 crave pull out/target/product/*/*.zip
